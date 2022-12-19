c7a3a637987644b7e3165052617e6f8b611c957d
Jay <jaypithava@xhtmljunkies.com>
2022-12-19 10:02:56 +0530

    like dislike count added

--- src/components/Post/Components/Footer/index.js
+++ src/components/Post/Components/Footer/index.js
@@ -1,23 +1,42 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, TouchableWithoutFeedback} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import styles from './styles';
 import Adicon from 'react-native-vector-icons/AntDesign';
 import Fontisto from 'react-native-vector-icons/Fontisto';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
-const Footer = ({LikesCount, caption, postedAt}) => {
+const Footer = ({LikesCount: likesCountProps, caption, postedAt}) => {
+  const [isLike, setIsLiked] = useState(false);
+  const [likescount, setlikescount] = useState(false);
+
+  const onLikePress = () => {
+    const amout = isLike ? -1 : 1;
+    setIsLiked(!isLike);
+    setlikescount(likescount + amout);
+  };
+
+  useEffect(() => {
+    setlikescount(likesCountProps);
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.iconsContainer}>
         <View style={styles.leftIcon}>
-          <Adicon name="hearto" size={25} color={'#545454'} />
+          <TouchableWithoutFeedback onPress={onLikePress}>
+            {isLike ? (
+              <Adicon name="heart" size={25} color={'#ff3434'} />
+            ) : (
+              <Adicon name="hearto" size={25} color={'#545454'} />
+            )}
+          </TouchableWithoutFeedback>
           <Fontisto name="comment" size={23} color={'#545454'} />
           <Ionicons name="paper-plane-outline" size={25} color={'#545454'} />
         </View>
         <FontAwesome name="bookmark-o" size={25} color={'#545454'} />
       </View>
-      <Text style={styles.like}>{LikesCount} Likes</Text>
+      <Text style={styles.like}>{likescount} Likes</Text>
       <Text style={styles.caption}>{caption}</Text>
       <Text style={styles.postedAt}>{postedAt}</Text>
     </View>
